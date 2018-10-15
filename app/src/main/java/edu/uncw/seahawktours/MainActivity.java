9ac4540406c7263791d96ed85e785b0f0aa62e7b
rmcal <rmc1890@uncw.edu>
2018-10-14 21:24:28 -0400

    Got the first activity started and 2nd activity works when clicking button

--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.uncw.seahawktours;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +13,23 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//Clicking the  button makes the new activity to pop up with a picture and details of the building selected
+public void onClickFindBuilding(View view)
+    {
+        //Get a reference to the Spinner
+        Spinner buildingName = (Spinner) findViewById(R.id.buildingsSpinner);
+
+        String buildInfo = String.valueOf(buildingName.getSelectedItem());
+
+        //Create an intent for DetailActivity
+        Intent intent = new Intent(this, DetailActivity.class);
+
+        //
+        //intent.putExtra(DetailActivity.EXTRA_MESSAGE, ???)
+
+        //Start DetailActivity with the intent
+        startActivity(intent);
+    }
+
 }

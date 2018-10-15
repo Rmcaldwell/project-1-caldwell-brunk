b1f74784ed83d86f7554539c054c0fae3b471841
David <dgb1056@uncw.edu>
2018-10-14 23:00:56 -0400

    Passed data to second activity

--- app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class DetailActivity extends Activity {
 
     public static final String EXTRA_MESSAGE = "spinnerText";
@@ -21,21 +23,25 @@ public class DetailActivity extends Activity {
         TextView buildingInfoView = (TextView) findViewById(R.id.buildingInfo);
         ImageView buildingImageView = (ImageView) findViewById(R.id.buildingImage);
         TextView buildingNameView = (TextView) findViewById(R.id.buildingName);
+        TextView captionView = (TextView) findViewById(R.id.caption);
 
         if (information.equals("CIS Building")) {
             buildingNameView.setText(R.string.CISBuildingName);
             buildingImageView.setImageResource(R.drawable.cis);
             buildingInfoView.setText(R.string.CISBuildingInfo);
+            captionView.setText(R.string.CISCaption);
         }
         else if (information.equals("Cameron Hall")) {
             buildingNameView.setText(R.string.CameronHallName);
             buildingImageView.setImageResource(R.drawable.cameron);
             buildingInfoView.setText(R.string.CameronHallInfo);
+            captionView.setText(R.string.CameronCaption);
         }
         else if (information.equals("Friday Hall")) {
             buildingNameView.setText(R.string.FridayHallName);
             buildingImageView.setImageResource(R.drawable.friday);
             buildingInfoView.setText(R.string.FridayHallInfo);
+            captionView.setText(R.string.FridayCaption);
         }
     }
 }

218259e531375ff7e3c75c782148d83c9fd628ef
David <dgb1056@uncw.edu>
2018-10-23 12:59:52 -0400

    Added Scroll view and caption text view

--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity {

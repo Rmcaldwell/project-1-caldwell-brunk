944d8ae4763d8b40e999a1604ab0ffb87cfe681c
David <dgb1056@uncw.edu>
2018-10-30 17:38:10 -0400

    Added Scroll view and caption text view

---
+++ app/src/main/java/edu/uncw/seahawktours/Building.java
@@ -0,0 +1,36 @@
+package edu.uncw.seahawktours;
+
+import android.media.Image;
+
+public class Building {
+
+    //MARK: Properties
+    private String name;
+    private String description;
+    private String url;
+    private int img_resourceID;
+    private String caption;
+
+
+    //
+    public Building(String name, String description, String url,
+                    int img_resourceID, String caption) {
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.img_resourceID = img_resourceID;
+        this.caption = caption;
+
+    }
+
+    public String getName() {
+
+        return this.name;
+    }
+
+    public String getDescription() {
+
+        return this.description;
+    }
+
+}
